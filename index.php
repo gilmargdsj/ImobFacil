0313bf32ec0f6042910c6e50d19ad212ab1997c0
Gilmar Gomes de Souza Junior <gilmargdsj@gmail.com>
2015-04-12 12:22:58 -0300

    testes
    testes

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+  <head>
+  <meta http-equiv="content-type" content="text/html; charset=windows-1250">
+  <meta name="generator" content="PSPad editor, www.pspad.com">
+  <title></title>
+  </head>
+  <body>
+   <?php
+    echo "XXX"; echo "<p></p>";
+    
+    print "xxx"; echo "<p></p>";
+    
+    $x=5;
+    $y=3;
+    
+    echo $x + $y; echo "<p></p>"; 
+    
+    $teste="dsjkfhksjdhfkjshfhksdf";
+    echo "<font size=5 face="."console".">".$teste."</font>";
+    
+    
+    
+   ?>
+  </body>
+</html>
